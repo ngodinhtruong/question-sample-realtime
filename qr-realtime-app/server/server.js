3269230a9aac0be0baca0ecbfbae6e9d02960b27
Zsaber <Ngodinh2339@gmail.com>
2026-03-18 23:57:21 +0700

    update folder

--- qr-realtime-app/server/server.js
+++ qr-realtime-app/server/server.js
@@ -110,8 +110,8 @@ const server = http.createServer(app);
 const io = socket.init(server, db);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 db.init();
